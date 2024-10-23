aa75b2bf87019598919f5e504ecc3772a9a97187
Akshat Jain <realakshatjain@gmail.com>
2024-10-23 13:13:30 +0530

    utils added

---
+++ client/src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,8 @@
+export interface CustomUser {
+  id?: string | null;
+  name?: string | null;
+  email?: string | null;
+  image?: string | null;
+  provider?: string | null;
+  token?: string | null;
+}
