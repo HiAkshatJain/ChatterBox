0e38a10c64b09fbc8c6aa5c23b4aca3bc1ca750d
Akshat Jain <realakshatjain@gmail.com>
2024-10-23 13:53:39 +0530

    footer added

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import localFont from "next/font/local";
 import { Inter as FontSans } from "next/font/google";
 import "./globals.css";
 import { cn } from "@/lib/utils";
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,13 @@
-export default function Home() {
-  return <div>Hellow</div>;
+import { getServerSession } from "next-auth";
+import { authOptions, CustomSession } from "./api/auth/[...nextauth]/options";
+import Footer from "@/components/base/Footer";
+
+export default async function Home() {
+  const session: CustomSession | null = await getServerSession(authOptions);
+  return (
+    <div className="min-h-screen flex flex-col ">
+      {/* Footer */}
+      <Footer />
+    </div>
+  );
 }
